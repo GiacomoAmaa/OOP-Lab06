3dae7ec615580adc651bb57c0126a01734508fc1
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2021-12-07 13:02:05 +0100

    Code refactoring to use exceptions

--- src/it/unibo/oop/lab/exception1/BaseRobotTest.java
+++ src/it/unibo/oop/lab/exception1/BaseRobotTest.java
@@ -2,6 +2,7 @@ package it.unibo.oop.lab.exception1;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
@@ -33,27 +34,35 @@ public final class BaseRobotTest {
         /*
          * 2) Move the robot right until it touches the world limit
          */
-        for (int i = 0; i < RobotEnvironment.WORLD_X_UPPER_LIMIT; i++) {
-            // check if position if coherent
-            assertTrue("[CHECKING MOVING RIGHT]", r1.moveRight());
+        try {
+        	for (int i = 0; i < RobotEnvironment.WORLD_X_UPPER_LIMIT; i++) {
+            r1.moveRight();
+            }
+        	// reached the right limit of the world
+        	r1.moveRight();
+        	fail("not allowed to go any further");
+        } catch (PositionOutOfBoundsException e ) {
+        	assertTrue(e.getMessage().contains("pos(" + (RobotEnvironment.WORLD_X_UPPER_LIMIT + 1) + ", 0)"));
         }
-        // reached the right limit of the world
-        assertFalse("[CHECKING MOVING RIGHT]", r1.moveRight());
+       
         // checking positions x=50; y=0
-        assertEquals("[MOVING RIGHT ROBOT POS X]", RobotEnvironment.WORLD_X_UPPER_LIMIT, r1.getEnvironment().getCurrPosX());
+        
         assertEquals("[MOVING RIGHT ROBOT POS Y]", 0, r1.getEnvironment().getCurrPosY());
         /*
          * 2) Move to the top until it reaches the upper right conrner of the world
          */
-        for (int i = 0; i < RobotEnvironment.WORLD_Y_UPPER_LIMIT; i++) {
+        try {
+        	for (int i = 0; i < RobotEnvironment.WORLD_Y_UPPER_LIMIT; i++) {
             // check if position if coherent
-            assertTrue("[CHECKING MOVING UP]", r1.moveUp());
+             r1.moveUp();
+            }
+        	// reached the upper limit of the world
+        	r1.moveUp();
+        	fail("not allowed to go any further");
+        } catch(PositionOutOfBoundsException e) {
+        	assertTrue(e.getMessage().contains("pos(" + RobotEnvironment.WORLD_X_UPPER_LIMIT + 
+        			", " + (RobotEnvironment.WORLD_Y_UPPER_LIMIT + 1) + ")"));
         }
-        // reached the upper limit of the world
-        assertFalse("[CHECKING MOVING UP]", r1.moveUp());
-        // checking positions x=50; y=80
-        assertEquals("[MOVING RIGHT ROBOT POS X]", RobotEnvironment.WORLD_X_UPPER_LIMIT, r1.getEnvironment().getCurrPosX());
-        assertEquals("[MOVING RIGHT ROBOT POS Y]", RobotEnvironment.WORLD_Y_UPPER_LIMIT, r1.getEnvironment().getCurrPosY());
     }
 
     /**
