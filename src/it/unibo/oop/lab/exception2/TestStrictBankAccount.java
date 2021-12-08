997140f782fcccb07909ed150e4e0a690726d347
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2021-12-08 15:49:33 +0100

    added tests for simple bank account

--- src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
+++ src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.lab.exception2;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 /**
@@ -14,7 +17,7 @@ public final class TestStrictBankAccount {
      */
     @Test
     public void testBankOperations() {
-        /*
+    	/*
          * 1) Creare due StrictBankAccountImpl assegnati a due AccountHolder a
          * scelta, con ammontare iniziale pari a 10000 e nMaxATMTransactions=10
          * 
@@ -23,5 +26,50 @@ public final class TestStrictBankAccount {
          * presenza di un id utente errato, oppure al superamento del numero di
          * operazioni ATM gratuite.
          */
+    	AccountHolder pers1  = new AccountHolder("Mario","Rossi",1) ;
+    	AccountHolder pers2  = new AccountHolder("Luigi","Bianchi",2) ;
+    	StrictBankAccount acc1 = new StrictBankAccount(pers1.getUserID(), 10000, 10);
+    	StrictBankAccount acc2 = new StrictBankAccount(pers2.getUserID(), 10000, 10);
+    	
+    	try {
+    		//test if not enough founds is thrown correctly
+    		acc1.withdraw(pers1.getUserID(), 15000);
+    	} catch (WrongAccountHolderException e) {
+    		fail("this is not the issue here");
+    	} catch (NotEnoughFoundsException e) {
+    		assertNotNull(e);
+    	} 
+    	
+    	for (int i = 0; i < 10; i++ ) {
+    		try {
+    			// there are no exceptions here
+    		    acc1.depositFromATM(pers1.getUserID(), 10);
+    	        } catch (WrongAccountHolderException e) {
+    	        	fail("there is no issue here");
+    	        } catch (TransactionsOverQuotaException e) {
+    		        fail("there is no issue here");
+    	        } 
+    	}
+    	
+    	 try {
+    		 // exceed the maximum allowed transactions
+    		 acc1.depositFromATM(pers1.getUserID(), 10);
+    		 fail();
+    	 } catch (WrongAccountHolderException e) {
+     		fail("there is no issue here");
+     	} catch (TransactionsOverQuotaException e) {
+     		assertNotNull(e);
+     	} 
+    	 
+    	 try {
+    		 // wrong id 
+             acc2.deposit(4, 100);
+             fail();
+         } catch (WrongAccountHolderException e) {
+             assertNotNull(e);
+         }
+    	
+    	}
+        
     }
-}
+
